d522c76bbcf28169a3735f4d28ce55707f21ad6c
tapnicom <admin@tapni.co>
2024-08-11 16:02:24 +0200

    - quote template v2 - graphql like response for a quote

--- demos/hybrid/astro.config.mjs
+++ demos/hybrid/astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import awsAmplify from "astro-aws-amplify";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   trailingSlash: "ignore",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react(),
+    tailwind()
+  ],
   output: "hybrid",
-  adapter: awsAmplify(),
+  adapter: awsAmplify()
 });
