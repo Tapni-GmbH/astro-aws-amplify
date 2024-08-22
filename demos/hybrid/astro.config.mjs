167ab3f175ee1c20a2a0f1c38e3157432015eda4
tapnicom <admin@tapni.co>
2024-08-22 18:57:08 +0200

    - vue removed - try #10

--- demos/hybrid/astro.config.mjs
+++ demos/hybrid/astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import awsAmplify from "astro-aws-amplify";
-import vue from '@astrojs/vue';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   trailingSlash: "ignore",
-  integrations: [mdx(), sitemap(), vue(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "hybrid",
   adapter: awsAmplify()
 });
